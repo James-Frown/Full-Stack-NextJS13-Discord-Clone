cffa0e05c87e9c301d7639859f12f652db15124e
James-Frown <21122@ctca.co.za>
2023-09-20 13:16:37 +0200

    update(timestamp: 1:40:30): Fullstack Discord Clone: Next.js 13, React, Socket.io, Prisma, Tailwind, MySQL

--- discord-v3/components/models/initial-Model.tsx
+++ discord-v3/components/models/initial-Model.tsx
@@ -13,8 +13,20 @@ import {
   DialogFooter,
   DialogHeader,
   DialogTitle,
-  DialogTrigger,
 } from "@/components/ui/dialog";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+import { Input } from "@/components/ui/input";
+
+import { Button } from "@/components/ui/button";
 
 const formSchema = z.object({
   name: z.string().min(1, {
@@ -52,6 +64,34 @@ export const InitialModel = () => {
             it later!
           </DialogDescription>
         </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <div className="space-y-8 px-6">
+              <div className="flex items-center justify-center text-center">
+                Todo: Image Upload
+              </div>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="uppercase text-xs font-bold text-zinc-600 dark:text-secondary/70">
+                      Server Name
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        disabled={isLoading}
+                        className="bg-zinc-400/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
+                        placeholder="Enter Server Name"
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+            </div>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   );
