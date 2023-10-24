700bf48377afcbfafbb70f9c0b0e46de2640b41f
James-Frown <21122@ctca.co.za>
2023-10-24 20:53:15 +0200

    feat(timestamp 6:50:00) Server Members Modal finnished

--- discord-v3/components/models/create-channel-modal.tsx
+++ discord-v3/components/models/create-channel-modal.tsx
@@ -13,6 +13,7 @@ import { useModal } from "@/hooks/use-modal-store";
 import { useParams, useRouter } from "next/navigation";
 
 import { useForm } from "react-hook-form";
+import { useEffect } from "react";
 
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
@@ -55,19 +56,28 @@ const formSchema = z.object({
 });
 
 export const CreateChannelModal = () => {
-  const { isOpen, onClose, type } = useModal();
+  const { isOpen, onClose, type, data } = useModal();
   const router = useRouter();
   const params = useParams();
 
   const isModalOpen = isOpen && type === "createChannel";
+  const { channelType } = data;
 
   const form = useForm({
     resolver: zodResolver(formSchema),
     defaultValues: {
       name: "",
-      type: ChannelType.TEXT,
+      type: channelType || ChannelType.TEXT,
     },
   });
+
+  useEffect(() => {
+    if (channelType) {
+      form.setValue("type", channelType);
+    } else {
+      form.setValue("type", ChannelType.TEXT);
+    }
+  }, [channelType, form]);
 
   const isLoading = form.formState.isSubmitting;
 
