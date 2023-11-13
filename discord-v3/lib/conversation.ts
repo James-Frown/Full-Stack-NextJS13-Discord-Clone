e33db26681863952e60f78b440aadb4cd585f7f4
James-Frown <21122@ctca.co.za>
2023-11-13 09:49:10 +0200

    feat(timestamp: 8:05:50) build out the conversations chat header logic and now is functional, working on socket.io setup and install

--- discord-v3/lib/conversation.ts
+++ discord-v3/lib/conversation.ts
@@ -1,17 +1,24 @@
 import { db } from "@/lib/db";
 
+export const getOrCreateConversation = async (
+  memberOneId: string,
+  memberTwoId: string
+) => {
+  let conversation =
+    (await findConversation(memberOneId, memberTwoId)) ||
+    (await findConversation(memberTwoId, memberOneId));
+
+  if (!conversation) {
+    conversation = await createNewConversation(memberOneId, memberTwoId);
+  }
+  return conversation;
+};
+
 const findConversation = async (memberOneId: string, memberTwoId: string) => {
   try {
     return await db.conversation.findFirst({
       where: {
-        AND: [
-          {
-            memberOneId: memberOneId,
-          },
-          {
-            memberTwoId: memberTwoId,
-          },
-        ],
+        AND: [{ memberOneId: memberOneId }, { memberTwoId: memberTwoId }],
       },
       include: {
         memberOne: {
